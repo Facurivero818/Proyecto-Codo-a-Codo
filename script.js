3931129a811af5386e07a83eda95981b491b8fc5
astorniolo <astorniolo@gmail.com>
2023-05-26 15:23:53 -0300

    validacion js

--- script.js
+++ script.js
@@ -19,3 +19,31 @@ ida.addEventListener("change", () => {
       regreso.style.display = "block";
     }
   });
+
+  function validarFormularioContacto() {
+    // validacion de nombre
+    let nombre = document.getElementById("nombreContacto").value.trim();
+      
+   
+
+    // Verificar si el nombre contiene solo caracteres alfabéticos y espacios
+    for (var i = 0; i < nombre.length; i++) {
+      var charCode = nombre.charCodeAt(i);
+      if (!((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || charCode === 32)) {
+        alert("El campo 'nombre' solo puede contener caracteres alfabéticos y espacios.");
+        return false;
+      }
+    }
+     // validacion de mail
+    let email = document.getElementById("emailContacto").value.trim();
+    let validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+  
+    if( ! validEmail.test(email) ){
+      alert('Email invalido, complete un email valido antes de continuar');
+      return false;
+    }
+    
+    // Si todas las validaciones son exitosas, enviar el formulario
+    alert("Formulario enviado correctamente.");
+    return true;
+  }
