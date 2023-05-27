c0961dda3c8354cdc89d0cd2077b2a4a959e9f0b
Facundo <facundorivero1991@gmail.com>
2023-05-27 16:17:06 -0300

    detalles

--- script.js
+++ script.js
@@ -22,17 +22,20 @@ ida.addEventListener("change", () => {
 
   //PEDIR DATOS A API, NO PUDIMOS CONSUMIR UNA API POR PROBLEMAS DE CORS, POR ESO CREAMOS UN ARCHIVO JSON CON DICHOS DATOS Y LO CONSUMIMOS
   const datos = async ()=>{
-    let info = await fetch('json.txt');
-    let resultado = await info.json();
-    console.log(resultado.airports[0].name)
+    try{
+      let info = await fetch('json.txt');
+      let resultado = await info.json();
+      console.log(resultado.airports[0].name)
     
-    let opciones = '';
-    for (aeropuerto of resultado.airports){
-      opciones += `<option value= '${aeropuerto.name}'>'${aeropuerto.name}'</option>`;
-    } 
-    salida.innerHTML= opciones;
-    vuelta.innerHTML= opciones;
-
+      let opciones = `<option value="Selec aeropuerto">Selec aeropuerto</option>`;
+      for (aeropuerto of resultado.airports){
+        opciones += `<option value= '${aeropuerto.name}'>'${aeropuerto.name}'</option>`;
+      } 
+      salida.innerHTML= opciones;
+      vuelta.innerHTML= opciones;
+    } catch(error){
+      console.log('Ocurrió un error:', error); 
+    }
   };
 
   const salida = document.getElementById('aeropuertoSalida');
@@ -40,28 +43,6 @@ ida.addEventListener("change", () => {
 
   datos()
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // VALIDACION FORMULARIO
